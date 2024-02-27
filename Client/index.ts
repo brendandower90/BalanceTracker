78116b5b230d64d91296fc40a9e6049e2a30f3f3
Brendan Dower <brendan@dower.id.au>
2024-02-27 17:31:55 +1100

    rename coinSchema to coinData rename userSchema to userData

--- Client/index.ts
+++ Client/index.ts
@@ -14,7 +14,7 @@ const erc20Abi = require('erc-20-abi');
 const myAddresses = ['0x43830Cfcf8332FD82D6490217f899d69f74803e1','0x9f3B3BBdC3A7D58054500b8e105a781fFd6FBedD'];
 const myTokens: TokenMap = {
     'LINK': '0x514910771AF9Ca656af840dff83E8264EcF986CA',
-    'GALA': '0xd1d2Eb1B1e90B638588728b4130137D262C87cae'
+    'GALA': '0xd1d2Eb1B1e90B638588728b4130137D262C87cae',
 }
 
 async function main() {
